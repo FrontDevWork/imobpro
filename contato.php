6e0f3567713653bd028eb0993876660fb07aab96
Gatojo <gatojogu1@gmail.com>
2020-03-09 19:24:47 -0300

    altercao para PHP

---
+++ contato.php
@@ -0,0 +1,43 @@
+<?php include "header.php" ?>
+    <section class="contato-box">
+
+        <div class="container">
+
+            <div class="row">
+                <div class="col-sm-12">
+                    <h2>Contato</h2>
+                </div>
+            </div>
+
+            <form>
+                <div class="row">
+                    <div class="col-sm-6 mb-4 d-flex justify-content-center">
+
+                        <input type="text" class="form-control input-contato" placeholder="Nome">
+                    </div>
+                    <div class="col-sm-6 mb-4 d-flex justify-content-center">
+
+                        <input type="text" class="form-control input-contato " placeholder="Cidade">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-6 mb-4 d-flex justify-content-center">
+                        <input type="text" class="form-control input-contato" placeholder="Telefone">
+                    </div>
+                    <div class="col-sm-6 mb-4 d-flex justify-content-center">
+
+                        <input type="text" class="form-control input-contato" placeholder="E-mail">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-6"></div>
+                    <div class="col-sm-6 d-flex justify-content-center">
+                        <button class="btn btn-enviar">Enviar</button>
+                    </div>
+                </div>
+            </form>
+        </div>>
+
+    </section>
+
+    <?php include "footer.php" ?>
---
+++ header.php
@@ -0,0 +1,48 @@
+<!doctype html>
+<html lang="PT-BR">
+
+<head>
+    <title>Home</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+    <link  href="plugins/fontawesome-free/css/all.css" rel="stylesheet">
+    <link  href="plugins/fontawesome-free/css/brands.css" rel="stylesheet">
+    <link  href="plugins/fontawesome-free/css/solid.css" rel="stylesheet">
+</head>
+
+<body>
+
+    <nav class="navbar navbar-expand-lg nav-principal">
+        <a class="navbar-brand" href="index.php">
+            <div class="logo">
+                <b>IMOB<span>PRO</span></b>
+            </div>
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"><i class="fas fa-bars"></i></span>
+    </button>
+    <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+        <ul class="navbar-nav barra-navegacao">
+            <li class="nav-item item-navegacao">
+                <a class="nav-link" href="index.php"> Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item item-navegacao">
+                <a class="nav-link" href="empresa.php">Empresa</a>
+            </li>
+            <li class="nav-item item-navegacao">
+                <a class="nav-link" href="lista-imoveis.php">Imoveis</a>
+            </li>
+            <li class="nav-item item-navegacao">
+                <a class="nav-link" href="contato.php">Contato</a>
+            </li>
+        </ul>
+    </a>
+</div>
+</nav>
